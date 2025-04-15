ac41aaf948ff64a63d10281382025227243cd38b
Gabriel Adamszuk <g.nakagawazuke@gmail.com>
2025-04-15 20:25:02 -0300

    Exercício-12

---
+++ q12.py
@@ -0,0 +1,4 @@
+# Faça um algoritmo que transforme uma velocidade fornecida em m/s pelo usuário para
+# Km/h. Para tal, multiplique o valor em m/s por 3,6.
+v = float(input("Insira a velocidade em m/s: "))
+print("A velocidade em Km/h é: ", v *3,6)
